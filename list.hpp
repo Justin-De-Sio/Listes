23bb5f637c90ad462a61c95d6c8b35ae4542d4f3
Justin-De-Sio <desio.j@live.fr>
2021-09-19 18:41:11 +0200

    La donnée membre m_Head change de sémantique : elle ne pointe plus vers la tête réelle mais vers une tête fictive nommée m_fictionaHead.

--- list.hpp
+++ list.hpp
@@ -1,113 +1,128 @@
-#pragma once
+#ifndef LIST_HPP
+#define LIST_HPP
+#include <iostream>
 #include "node.hpp"
 
-template<typename T>
-
+template <typename T>
 class CList
 {
 private:
-    CNode<T>* m_Head; //  premier élément
-    CNode<T>* m_Queue; // dernier élément
+    CNode<T> *m_fictionalHead;
+    CNode<T> *m_realTail;
+
 public:
     CList();
     ~CList();
-    //ajout en tête de liste
-    void push_front(const T& data);
-
-    //ajout en queue de liste
-    void push_back(const T& data);
-
-    //affichage
+    void push_front(const T &val);
     void Show() const;
-
-    //recherche d'un élément dans la liste, renvoie le pointeur du maillon si l'élément est présent, nullptr sinon
-    CNode<T>* Find(const T& data) const;
-
-    //ajout d'une valeur après un maillon de la liste
-    void Add(const T& data, CNode<T>*);
-
-    //détache un maillon de la liste et le supprime
-    void Delete(CNode<T>*);
-
-   
-
-    
+    void ShowAdvanced() const;
+    CNode<T> *Find(const T &val) const;
+    void Add(const T &val, CNode<T> *);
+    void Delete(CNode<T> *);
+    void push_back(const T &val);
+    CNode<T> *Begin() const;
+    void AddAfter(CNode<T> *, const T &val);
 };
 
-template<typename T>
- CList<T>::CList() : m_Head(new CNode<T> ()),m_Queue(m_Head)
+template <typename T>
+CNode<T> *CList<T>::Begin() const
 {
+    return m_fictionalHead;
 }
 
- template<typename T>
- inline CList<T>::~CList()
- {
-     delete m_Head;
- }
+template <typename T>
+void CList<T>::AddAfter(CNode<T> *ptr, const T &val)
+{
+    if (ptr != nullptr)
+    {
+        ptr->SetNextNode(new CNode<T>(val, ptr->GetNextNode()));
+        if (ptr == m_realTail)
+            m_realTail = ptr->GetNextNode();
+    }
+    else
+        std::cout << "nombre pas dans la liste\n";
+}
 
- template<typename T>
- inline void CList<T>::push_front(const T& data)
- {
-     m_Head = new CNode<T>(data, m_Head);
-     if (m_Queue == nullptr)m_Queue = m_Head;
- }
+template <typename T>
+void CList<T>::Add(const T &val, CNode<T> *ptr)
+{
+    CNode<T> *ptrTmp = new CNode<T>(val, ptr->GetNextNode());
+    ptr->SetNextNode(ptrTmp);
+    if (ptr == m_realTail)
+        m_realTail = ptr->GetNextNode();
+}
 
- template<typename T>
- inline void CList<T>::push_back(const T& data)
- {
+template <typename T>
+CList<T>::CList() : m_fictionalHead(new CNode<T>()), m_realTail(m_fictionalHead){};
 
-     CNode<T>* ptrTemp = new CNode<T>(data, nullptr);
-     m_Queue->SetNextNode(ptrTemp);
-     m_Queue = ptrTemp;
-     
- }
+template <typename T>
+CList<T>::~CList()
+{
+    delete m_fictionalHead;
+}
 
- template<typename T>
- inline void CList<T>::Show() const
- {
-     //last data is pointer
-     cout << "|   HEAD   | address : " << m_Head << " |\n";
-     for(CNode<T>* ptr(m_Head);ptr!=nullptr;ptr=ptr->GetNextNode())
+template <typename T>
+CNode<T> *CList<T>::Find(const T &val) const
+{
+    CNode<T> *Ptr(m_fictionalHead->GetNextNode());
+    for (; Ptr != nullptr && Ptr->GetData() != val; Ptr = Ptr->GetNextNode())
+        ;
+
+    return Ptr;
+}
+
+template <typename T>
+void CList<T>::push_front(const T &val)
+{
+    m_fictionalHead->SetNextNode(new CNode<T>(val, m_fictionalHead->GetNextNode()));
+    if (m_realTail == m_fictionalHead)
+        m_realTail = m_fictionalHead->GetNextNode();
+}
+
+template <typename T>
+void CList<T>::Show() const
+{
+    //for (CNodeInt* Ptr (m_Head); Ptr; ++*Ptr)
+    for (CNode<T> *Ptr(m_fictionalHead->GetNextNode()); Ptr; Ptr = Ptr->GetNextNode())
+        std::cout << Ptr->GetData() << "; ";
+    std::cout << std::endl;
+}
+
+template <typename T>
+inline void CList<T>::ShowAdvanced() const
+{
+    //last data is pointer
+    cout << "|   HEAD FICTIVE   | address : " << m_fictionalHead << " |\n";
+    for (CNode<T> *ptr(m_fictionalHead->GetNextNode()); ptr != nullptr; ptr = ptr->GetNextNode())
         cout << "| data : " << ptr->GetData() << " | address : " << ptr << " | next address : " << ptr->GetNextNode() << " |\n";
-     cout << "|   queue  | address : " << m_Queue << " |\n";
-     //first data is Queue 
-     cout << "\n";
- }
+    cout << "|   queue  | address : " << m_realTail << " |\n";
+    //first data is Queue
+    cout << "\n";
+}
 
- template<typename T>
- inline CNode<T>* CList<T>::Find(const T& data) const
- {
-     CNode<T>* ptr(m_Head);
-     for (; ptr!= nullptr &&ptr->GetData() !=data; ptr = ptr->GetNextNode());
-     return ptr;
- }
+template <typename T>
+void CList<T>::Delete(CNode<T> *pDelete)
+{
+    if (pDelete == nullptr)
+        return;
+    CNode<T> *pFind(m_fictionalHead);
+    for (; pFind->GetNextNode() != pDelete; pFind = pFind->GetNextNode())
+        ;
 
- template<typename T>
- inline void CList<T>::Add(const T& data, CNode<T>* ptr)
- {
-     if (ptr != nullptr) {
-         CNode<T>* ptrTemp = new CNode<T>(data, ptr->GetNextNode());
-         ptr->SetNextNode(ptrTemp);
-     }
-     else
-         m_Head = new CNode<T>(data, m_Head);
-     
- }
+    pFind->SetNextNode(pDelete->GetNextNode());
+    if (pDelete == m_realTail)
+        m_realTail = pFind;
+    pDelete->SetNextNode(nullptr);
 
- template<typename T>
- inline void CList<T>::Delete(CNode<T>* ptr)
- {
-     if (ptr == m_Head) {
-         m_Head = ptr->GetNextNode();
-         ptr->SetNextNode(nullptr);
-         delete ptr;
-         return;
-     }
-     if (ptr == nullptr) return;
-     CNode<T>* ptrTemp(m_Head);
-     for (; ptrTemp->GetNextNode() != ptr; ptrTemp = ptrTemp->GetNextNode());
-     ptrTemp->SetNextNode(ptr->GetNextNode());
-     ptr->SetNextNode(nullptr); 
-     delete ptr;
+    delete pDelete;
+}
 
- }
+template <typename T>
+void CList<T>::push_back(const T &val)
+{
+    CNode<T> *pTmp = new CNode<T>(val, nullptr);
+    m_realTail->SetNextNode(pTmp);
+    m_realTail = pTmp;
+}
+
+#endif // LIST_HPP
--- main.cpp
+++ main.cpp
@@ -2,50 +2,96 @@
 #include "node.hpp"
 #include "list.hpp"
 using namespace std;
-void ListeSimple(void) {
-	CNode<int>* Head = nullptr;
+void ListeSimple(void)
+{
+	CNode<int> *Head = nullptr;
 	for (int i = 0; i < 5; i++)
 	{
 		Head = new CNode<int>(i, Head);
 	}
 
-	
-
-	for (CNode<int>* ptr(Head); ptr != nullptr; ptr = ptr->GetNextNode())
+	for (CNode<int> *ptr(Head); ptr != nullptr; ptr = ptr->GetNextNode())
 		cout << "| data : " << ptr->GetData() << " | address : " << ptr << " | next address : " << ptr->GetNextNode() << " |\n";
 	delete Head;
 }
-
-
-int main()
+void ListeSimpleV2()
 {
-	
-	
+
 	int i;
 	CList<int> liste;
 	for (int i = 1; i < 5; i++)
 	{
 		liste.push_front(i);
 	}
-	liste.Show();
-	
-	cout << "nombre a ajouter à la fin: ";
+	liste.ShowAdvanced();
+
+	cout << "nombre a ajouter ï¿½ la fin: ";
 	cin >> i;
 	liste.push_back(i);
-	liste.Show();
+	liste.ShowAdvanced();
 
-	
 	cout << "nombre a ajouter: ";
 	cin >> i;
-	CNode<int>* ptr = liste.Find(i);
-	liste.Add(i, ptr);
-	liste.Show();
+	CNode<int> *ptr = liste.Find(i);
+	liste.AddAfter(ptr, i);
+	liste.ShowAdvanced();
 
 	cout << ((ptr != NULL) ? " " : "non ") << "trouve" << endl;
 
 	cout << "nombre a supprime: ";
 	cin >> i;
 	liste.Delete(liste.Find(i));
-	liste.Show();
+	liste.ShowAdvanced();
+}
+void ListeSimpleV3()
+{
+	cout << "ListeSimpleV2 : \n\n";
 
+	CList<int> AList;
+
+	// Le dernier element cree est toujours le premier de la liste
+
+	for (unsigned i = 20; i; i -= 2)
+		AList.AddAfter(AList.Begin(), i);
+	cout << "hfejkrhg " << endl;
+
+	for (unsigned i(0); i < 5;)
+	{
+		AList.push_front(i++);
+	}
+
+	AList.Show();
+	//	AList.Show ();
+	/*	int i;
+		cin >>i;
+		CNodeInt* ptr = AList.Find (i);
+		AList.Add (3*i, ptr);
+		AList.Show ();
+
+		cout << ((ptr != NULL)?  " " : "non ") << "trouve" << endl;
+	*/
+	//cout << "valeur trouvï¿½e" << ptr -> GetData () << endl;
+
+	//delete AList;
+
+	/*CNodeInt* ptr = AList.Find (2);
+	cout << ((ptr != NULL)?  " " : "non ") << "trouve" << endl;
+	AList.Delete (ptr);
+	AList.Show ();*/
+
+	//AList.Delete (AList.Find (0));
+
+	AList.push_back(6);
+	AList.push_back(7);
+
+	AList.Delete(AList.Find(2));
+	AList.Delete(AList.Find(12));
+	AList.Delete(AList.Find(20));
+	AList.Show();
+	//delete ptr;
+	cout << endl;
+}
+int main()
+{
+	ListeSimpleV2();
 }
